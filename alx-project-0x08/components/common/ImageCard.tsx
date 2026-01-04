41885516a51f9a2e2c8c16c2e03802daed9f59d7
ZeeAzeez <azeezahaazeez@gmail.com>
2026-01-04 16:44:49 +0100

    updated project 0x08

--- alx-project-0x08/components/common/ImageCard.tsx
+++ alx-project-0x08/components/common/ImageCard.tsx
@@ -1,4 +1,4 @@
-import { GeneratedImageProps } from "../../interfaces";
+import { GeneratedImageProps } from "@/interfaces";
 
 const ImageCard: React.FC<GeneratedImageProps> = ({
   imageUrl,
@@ -6,5 +6,24 @@ const ImageCard: React.FC<GeneratedImageProps> = ({
   width,
   action,
 }) => {
-  return <div onClick={() => action(imageUrl)} className="mt-6 border"></div>;
+  return (
+    <div
+      onClick={() => action(imageUrl)}
+      className="mt-6 border hover:cursor-pointer"
+    >
+      <img
+        src={imageUrl}
+        alt={prompt}
+        className={`w-full max-w-md rounded-lg shadow-lg`}
+      />
+      <h2 className={`${width ? "text-sm" : "text-xl"} font-semibold mt-2`}>
+        Your Prompt:
+      </h2>
+      <p className={`${width ? "text-xs" : "text-lg"} text-gray-700 mb-4`}>
+        {prompt}
+      </p>
+    </div>
+  );
 };
+
+export default ImageCard;
